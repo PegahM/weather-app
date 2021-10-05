f4b9da68ed18a6cc008c9f96b225d70168c81a66
PegahM <pegahh.monzeri@gmail.com>
2021-10-05 20:28:04 +0330

    Displayed forecast for 6 upcoming day
    Displayed forecast for 6 upcoming day (included the minimum and maximum and weather icon for each day)

--- src/app.js
+++ src/app.js
@@ -20,30 +20,48 @@ function formatDate(timestamp) {
   let day = date.getDay();
   return `${days[day]} ${hours}:${minutes}`;
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday"];
+
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (i) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
               <div class="col-2">
-                <div class="wather-forecast-date">${i}</div>
+                <div class="wather-forecast-date">${formatDay(
+                  forecastDay.dt
+                )}</div>
 
                 <img
-                  src="https://openweathermap.org/img/wn/50d@2x.png"
+                  src="https://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
                   alt=""
                   width="42"
                 />
                 <div class="weather-forecst-temprature">
-                  <span class="weather-forecast-temprature-max"> 18° </span>
-                  <span class="weather-forecast-temprature-min"> 12° </span>
+                  <span class="weather-forecast-temprature-max"> ${Math.round(
+                    forecastDay.temp.max
+                  )}° </span>
+                  <span class="weather-forecast-temprature-min"> ${Math.round(
+                    forecastDay.temp.min
+                  )}° </span>
                 </div>
               </div>
             `;
+    }
   });
+
   forecastHTML = forecastHTML + `</div>`;
 
   forecastElement.innerHTML = forecastHTML;
